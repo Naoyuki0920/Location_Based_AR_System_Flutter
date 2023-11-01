0c9e86f762e3757f8a2fba67eb36a4434756d0fe
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-11-01 23:27:41 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/naoyuki/location_based_ar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naoyuki.location_based_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import geolocator_apple
+import network_info_plus
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
